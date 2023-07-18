2553ad4fb8e5ef17543e18442467feb0ce6e74bd
Austin Perrine <akperrine@gmail.com>
2023-07-18 11:08:10 -0700

    move out of shell folder

---
+++ data.ts
@@ -0,0 +1,28 @@
+const user = {
+  personalInfo: {
+    firstName: "",
+    lastName: "",
+    ssn: 1,
+    dob: Date,
+    location: 94115,
+    password: 1,
+  },
+
+  taxInfo: {
+    filingStatus: "Enum String",
+    formsW2: [
+      {
+        income: 1,
+        deductions: { name: 1 },
+      },
+    ],
+    forms1099: [
+      {
+        income: 1,
+        witheld: 1,
+      },
+    ],
+    return: 0,
+    taxPercentage: 0,
+  },
+};
